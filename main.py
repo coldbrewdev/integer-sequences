3fa4f297d71a35a5abc94c54ab1dc218995493d6
coldbrewdev <kenny82988@gmail.com>
2021-02-08 19:51:53 -0600

    Added Mersenne Primes Improved is_prime

--- main.py
+++ main.py
@@ -25,6 +25,21 @@ def force_integer(string):
     return string
 
 
+def is_integer(m):
+    return bool(m == int(m))
+
+
+def log(base, m):
+    """Returns integer logs or False"""
+    p = 0
+    while is_integer(m) and m > 1:
+        m = m/base
+        p += 1
+    if m == 1:
+        return p
+    return False
+
+
 def is_abundant(n):
     return bool(sum(divisors(n)) > 2*n)
 
@@ -41,8 +56,14 @@ def abundant_list(m):
     return abundants
 
 
-def is_prime(n):
-    return bool(len(divisors(n)) == 2)
+def is_prime(m):
+    if m == 1:
+        return False
+    x = int(math.sqrt(m))+1
+    for i in range(2, x):
+        if m % i == 0:
+            return False
+    return True
 
 
 def prime_list(m):
@@ -54,16 +75,36 @@ def prime_list(m):
             primes.append(n)
             i += 1
         n += 1
-    print(primes)
+    return primes
+
+
+def is_mersenne(m):
+    return all([is_prime(m), is_prime(log(2, m+1))])
+
+
+def mersenne_list(m):
+    if m > 8:
+        m = 8
+        print('Sorry, for the sake of processing power this program maxes at 8 Mersenne Primes. Feel free to open the '
+              'code and remove this limit. Calculating the first 8 Mersenne Primes...')
+    i = 0
+    n = 1
+    mersenne = []
+    while i < m:
+        if is_mersenne(2**n-1):
+            mersenne.append(2**n-1)
+            i += 1
+        n += 1
+    return mersenne
 
 
 def menu():
-    s = ['prime', 'abundant']
-    f_list = [is_prime, is_abundant]
-    g_list = [prime_list, abundant_list]
+    s = ['prime', 'abundant', 'mersenne prime']
+    f_list = [is_prime, is_abundant, is_mersenne]
+    g_list = [prime_list, abundant_list, mersenne_list]
     sequence_menu = 'What sequences are you interested in?\n'
     for i in range(len(s)):
-        sequence_menu += str(i+1) + '. ' + s[i].capitalize() + ' Numbers\n'
+        sequence_menu += str(i+1) + '. ' + s[i].title() + ' Numbers\n'
     x, y = '', ''
     while x not in range(1, len(s)+1):
         x = int(force_integer(input(sequence_menu)))
@@ -87,7 +128,3 @@ def menu():
 
 if __name__=='__main__':
     menu()
-
-
-
-
